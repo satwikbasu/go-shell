66180873178c10b11a223c62be089a6849372595
satwik-100 <satwikbasu100@gmail.com>
2026-03-30 20:19:17 +0000

    feat: add executable check in $PATH -type <command> is validated for presence in $PATH -<command> is checked for executable permissions and state -appropriate message is printed with full path of executable e.g. "type cat" outputs to "cat is /bin/cat"

--- app/main.go
+++ app/main.go
@@ -10,14 +10,15 @@ import (
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		// Print the shell prompt
-		fmt.Print("$ ")
-		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("$ ")
 		command, err := reader.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		command = strings.TrimSpace(command) // remove trailing whitespaces
 		builtIn := []string{"echo", "exit", "type"} // slice to check for built-in shell commands
 
@@ -30,7 +31,8 @@ func main() {
 		
 		// echo command parse and eval
 		} else if strings.HasPrefix(command, "echo") {
-			fmt.Printf("%s\n", command[5:])
+			echoPrint := strings.Join(parts[1:], " ")
+			fmt.Printf("%s\n", echoPrint)
 		
 		// type command parse and eval
 		} else if strings.HasPrefix(command, "type") {
@@ -42,7 +44,34 @@ func main() {
 				if slices.Contains(builtIn, argAfterType) {
 					fmt.Printf("%s is a shell builtin\n", argAfterType)
 				} else {
-					fmt.Printf("%s: not found\n", argAfterType)
+					pathValue := os.Getenv("PATH") // stores $PATH as a string
+					directories := strings.Split(pathValue, string(os.PathListSeparator)) // splits $PATH directories into a slice
+					// fmt.Printf("%#v\n", directories)
+					// fmt.Printf("%T\n", pathValue)
+
+					var execFound bool // to check if executable was found with right permissions
+
+					for _, dir := range directories {
+						fullPath := dir + "/" + argAfterType
+						fileInfo, err := os.Stat(fullPath)
+						// fmt.Printf("%#v\n", fullPath)
+						if err == nil {
+							mode := fileInfo.Mode()
+							// fmt.Printf("%#v\n", mode)
+							// Check if the found file is not a directory and has any execute permissions set (owner, group, or others)
+							// mode.IsDir() returns true if the file is a directory, so !mode.IsDir() ensures it's a file
+							// mode.Perm()&0111 checks if any of the execute bits are set (octal 0111 = execute for owner, group, or others)
+							if !mode.IsDir() && (mode.Perm()&0111 != 0) {
+								// Found an executable file in the directory
+								fmt.Printf("%s is %s\n", argAfterType, fullPath)
+								execFound = true
+								break
+							}
+						}
+					}
+					if !execFound {
+						fmt.Printf("%s: not found\n", argAfterType)
+					}
 				}
 			}
 		
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/codecrafters-io/shell-starter-go
 
-go 1.26.0
+go 1.22.2
